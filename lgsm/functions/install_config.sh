6f2e6a2bbf6d58838d17a2a1aa75c376d964a73b
Alexander Hurd <hurdad@gmail.com>
2017-02-13 17:32:31 -0500

    update

--- lgsm/functions/install_config.sh
+++ lgsm/functions/install_config.sh
@@ -204,6 +204,12 @@ elif [ "${gamename}" == "Codename CURE" ]; then
 	fn_fetch_default_config
 	fn_default_config_remote
 	fn_set_config_vars
+elif [ "${gamename}" == "Counter-Strike Beta" ]; then
+	gamedirname="CounterStrikeBeta"
+	array_configs+=( server.cfg )
+	fn_fetch_default_config
+	fn_default_config_remote
+	fn_set_config_vars
 elif [ "${gamename}" == "Counter-Strike 1.6" ]; then
 	gamedirname="CounterStrike"
 	array_configs+=( server.cfg )
