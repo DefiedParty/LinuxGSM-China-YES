d9a393a7caa1cc851d7cc5c46f142487caeeae4a
Daniel Gibbs <me@danielgibbs.co.uk>
2019-12-01 22:21:26 +0000

    testing curl loop

--- lgsm/functions/core_dl.sh
+++ lgsm/functions/core_dl.sh
@@ -133,11 +133,14 @@ fn_fetch_file(){
 			echo -en "downloading ${local_filename}..."
 			fn_sleep_time
 			echo -en "\033[1K"
-			curlcmd=$(curl --progress-bar --retry 10 --retry-delay 3 --fail -L -C - -o "${local_filedir}/${local_filename}" "${remote_fileurl}")
+			while [ "${curlexitcode}" != 0 ]||[ -z "${curlexitcode}" ]; do
+				curlcmd=$(curl -v --retry 10 --retry-delay 3 --fail -L -C - -o "${local_filedir}/${local_filename}" "${remote_fileurl}")
+				curlexitcode=$?
+			done
 			echo -en "downloading ${local_filename}..."
 		else
 			echo -en "    fetching ${local_filename}...\c"
-			curlcmd=$(curl -s --retry 3 --retry-delay 3 --fail -L -C - -o "${local_filedir}/${local_filename}" "${remote_fileurl}" 2>&1)
+			curlcmd=$(curl -s --fail -L -o "${local_filedir}/${local_filename}" "${remote_fileurl}" 2>&1)
 		fi
 		local exitcode=$?
 		if [ ${exitcode} -ne 0 ]; then
