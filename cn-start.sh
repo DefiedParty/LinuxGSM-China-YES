49dcd0818acb5a91dd208fd0e48d82e13d435475
DefiedParty <dp@dpii.club>
2020-08-22 19:12:07 +0800

    更新readme

--- cn-start.sh
+++ cn-start.sh
@@ -10,4 +10,4 @@ git clone https://gitee.com/DefiedParty/LinuxGSM
 cd LinuxGSM
 chmod +x linuxgsm.sh
 chmod -R 764 lgsm/functions
-echo "初始化完成，请继续按照教程执行！（补充，当前的工作目录为：/home/用户名/LinuxGSM，也就是说下文所有有关目录的命令都要在/home/用户名/ 后面加上LinuxGSM)"
+echo "初始化完成，请继续按照教程执行！（补充，请先cd LinuxGSM 切换工作目录为：/home/用户名/LinuxGSM，也就是说下文所有有关目录的命令都要在/home/用户名/ 后面加上LinuxGSM)"
